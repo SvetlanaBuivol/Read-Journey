499787ea852a0a3ec541a6c58b428e11eaf1b11b
SvetlanaBuuvol <divinesvetok@gmail.com>
2024-02-28 18:51:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode> 
-      <BrowserRouter>
+      <BrowserRouter basename='/read-journey'>
         <GlobalStyle />
         <App />
       </BrowserRouter>
