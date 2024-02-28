1dcce4b93550a6b4901645bfb20f6e0e8a251435
SvetlanaBuuvol <divinesvetok@gmail.com>
2024-02-28 23:31:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/App.jsx';
+import App from './components/App';
 import GlobalStyle from './css/globlStyles.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode> 
-      <BrowserRouter basename='/read-journey'>
+      <BrowserRouter basename='read-journey'>
         <GlobalStyle />
         <App />
       </BrowserRouter>
