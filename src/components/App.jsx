fe2b7609d0218469aaf40871e7aa03b971f0bc17
SvetlanaBuuvol <divinesvetok@gmail.com>
2024-02-28 23:50:49 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,23 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import Register from '../pages/Register';
-import Library from '../pages/Library';
-import Recommended from '../pages/Recommended';
-import Login from '../pages/Login';
+import { lazy } from 'react';
 import SharedLayout from '../components/SharedLayout/SharedLayout';
 import PrivateRoute from './Routs/PrivateRoute';
 import PublicRoute from './Routs/PublicRoute';
 
+const Register = lazy(() => import('../pages/Register'));
+const Login = lazy(() => import('../pages/Login'));
+const Recommended = lazy(() => import('../pages/Recommended'));
+const Library = lazy(() => import('../pages/Library'));
+
 function App() {
   return (
     <Routes>
-      <Route path="/read-journey/register" element={<PublicRoute><Register /></PublicRoute>} />
-      <Route path="/read-journey/login" element={<PublicRoute><Login /></PublicRoute>} />
+      <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
+      <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
       <Route path="/" element={<PrivateRoute><SharedLayout/></PrivateRoute>}>
-        <Route index element={<Navigate to="/recommended"/>}/>
-        <Route path="/recommended" element={<Recommended />} />
-        <Route path="/library" element={<Library />} />
+        <Route index element={<Navigate to="recommended"/>}/>
+        <Route path="recommended" element={<Recommended />} />
+        <Route path="library" element={<Library />} />
       </Route>
     </Routes>
   );
